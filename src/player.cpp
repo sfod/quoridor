aa22f3d83e70ef4b5b38bbd5517e830048b3e094
sfod <octosyllabic@gmail.com>
2013-10-01 23:21:47 +0400

    Remove Board dependency from Player module.

--- src/player.cpp
+++ src/player.cpp
@@ -1,20 +1,13 @@
 #include "player.hpp"
 
-#include <iostream>
-
 namespace Quoridor {
 
-Player::Player(const std::string &name) : name_(name), board_side_(-1), pos_()
+Player::Player(const std::string &name) : name_(name)
 {
 }
 
 Player::~Player()
 {
-}
-
-void Player::set_pos(const pos_t &pos)
-{
-    pos_ = pos;
 }
 
 }  /* namespace Quoridor */
