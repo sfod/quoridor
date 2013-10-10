bc18d4fff8dbdab3c89139aded6c24f3813a9f70
sfod <octosyllabic@gmail.com>
2013-10-10 14:28:44 +0400

    Add tests for Board add_wall method.

--- t/board_unittest.cpp
+++ t/board_unittest.cpp
@@ -4,7 +4,7 @@
 
 class BoardTest : public ::testing::Test {
 protected:
-    BoardTest() : board_(1, 2) {
+    BoardTest() : board_(8, 9) {
     }
 
     Quoridor::Board board_;
@@ -12,8 +12,8 @@ protected:
 
 TEST_F(BoardTest, ctor)
 {
-    EXPECT_EQ(1, board_.row_num());
-    EXPECT_EQ(2, board_.col_num());
+    EXPECT_EQ(8, board_.row_num());
+    EXPECT_EQ(9, board_.col_num());
     // test invalid Board construction
     EXPECT_THROW(Quoridor::Board(-1, 1), Quoridor::Exception);
     EXPECT_THROW(Quoridor::Board(8, 0), Quoridor::Exception);
@@ -32,6 +32,19 @@ TEST_F(BoardTest, set_size)
     board_.set_size(10, 11);
     EXPECT_EQ(10, board_.row_num());
     EXPECT_EQ(11, board_.col_num());
+}
+
+TEST_F(BoardTest, add_wall)
+{
+    // add valid walls
+    EXPECT_EQ(0, board_.add_wall(Quoridor::Wall(0, 0, 0, 2)));
+    EXPECT_EQ(0, board_.add_wall(Quoridor::Wall(0, 0, 2, 2)));
+
+    // add walls beyond the board
+    EXPECT_EQ(-1, board_.add_wall(Quoridor::Wall(0, 10, 0, 2)));
+
+    // add intersecting walls
+    EXPECT_EQ(-2, board_.add_wall(Quoridor::Wall(0, 0, 0, 1)));
 }
 
 int main(int argc, char **argv)
