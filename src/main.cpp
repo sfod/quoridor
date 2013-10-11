bc7f1564a3dbe936169703676d19fa168fc2c768
sfod <octosyllabic@gmail.com>
2013-10-11 15:26:37 +0400

    Use UI::Console to run game.

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,13 @@
 #include <iostream>
 #include <memory>
 
-#include "board.hpp"
-#include "game.hpp"
+#include "UI/console.hpp"
 #include "pawn.hpp"
 
 int main()
 {
-    Quoridor::Game game(std::shared_ptr<Quoridor::Board>(new Quoridor::Board(9, 9)));
-    game.add_pawn(std::shared_ptr<Quoridor::Pawn>(new Quoridor::Pawn("white")));
-    game.add_pawn(std::shared_ptr<Quoridor::Pawn>(new Quoridor::Pawn("black")));
-
-    game.main_loop();
+    Quoridor::UI::Console console(2);
+    console.run();
 
     return EXIT_SUCCESS;
 }
