3934bf9a5dd7de15fe881d0be46bed650fb617ef
sfod <octosyllabic@gmail.com>
2013-10-14 22:31:34 +0400

    Parse cmd with boost program_options.

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,43 @@
 #include <iostream>
 #include <memory>
 
+#include <boost/program_options.hpp>
+
 #include "UI/console.hpp"
 #include "pawn.hpp"
 
-int main()
+namespace po = boost::program_options;
+
+int main(int argc, char **argv)
 {
-    Quoridor::UI::Console console(2);
+    po::options_description options("Options");
+    options.add_options()
+        ("players,p", po::value<int>(), "number of players")
+        ("help,h", "show help message")
+        ;
+    po::variables_map vm;
+
+    try {
+        po::store(po::parse_command_line(argc, argv, options), vm);
+    }
+    catch (po::error &e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << options << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    int players = 2;
+    if (vm.count("players")) {
+        players = vm["players"].as<int>();
+    }
+
+    Quoridor::UI::Console console(players);
     console.run();
 
     return EXIT_SUCCESS;
