7a7ae4eb8606f362fe6c4e6ff267779e58343837
sfod <octosyllabic@gmail.com>
2013-10-17 22:40:59 +0400

    Add function init.

--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,37 @@
 
 namespace po = boost::program_options;
 
+
+struct game_opts_t {
+    int player_num;
+};
+
+
+int init(int argc, char **argv, game_opts_t *game_opts);
+
+
 int main(int argc, char **argv)
+{
+    game_opts_t game_opts;
+
+    if (init(argc, argv, &game_opts) < 0) {
+        return EXIT_FAILURE;
+    }
+
+    Quoridor::UI::Console console(game_opts.player_num);
+    console.run();
+
+    return EXIT_SUCCESS;
+}
+
+int init(int argc, char **argv, game_opts_t *game_opts)
 {
     po::options_description options("Options");
     options.add_options()
-        ("players,p", po::value<int>(), "number of players")
+        ("player-num,n", po::value<int>(&game_opts->player_num)->default_value(2),
+             "number of players")
         ("help,h", "show help message")
-        ;
+    ;
     po::variables_map vm;
 
     try {
@@ -24,23 +48,15 @@ int main(int argc, char **argv)
     }
     catch (po::error &e) {
         std::cerr << e.what() << std::endl;
-        return EXIT_FAILURE;
+        return -1;
     }
 
     po::notify(vm);
 
     if (vm.count("help")) {
         std::cout << options << std::endl;
-        return EXIT_FAILURE;
+        return -1;
     }
 
-    int players = 2;
-    if (vm.count("players")) {
-        players = vm["players"].as<int>();
-    }
-
-    Quoridor::UI::Console console(players);
-    console.run();
-
-    return EXIT_SUCCESS;
+    return 0;
 }
