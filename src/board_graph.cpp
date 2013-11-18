2cef11afd1b56132a1d384a043e4e7587703709e
sfod <octosyllabic@gmail.com>
2013-11-18 18:17:08 +0400

    Add graph representation of Board.

---
+++ src/board_graph.cpp
@@ -0,0 +1,106 @@
+#include "board_graph.hpp"
+
+namespace Quoridor {
+
+BoardGraph::BoardGraph(int row_num, int col_num)
+    : g_(row_num * col_num)
+{
+    for (int i = 0; i < row_num * col_num; ++i) {
+        nodes_.push_back(i);
+    }
+
+    int nidx;
+    for (int i = 0; i < row_num; ++i) {
+        for (int j = 0; j < col_num; ++j) {
+            if (j != 0) {
+                nidx = i * col_num + j - 1;
+                edges_.insert(edge(nodes_[i * col_num + j], nodes_[nidx]));
+            }
+            if (j != col_num - 1) {
+                nidx = i * col_num + j + 1;
+                edges_.insert(edge(nodes_[i * col_num + j], nodes_[nidx]));
+            }
+            if (i != 0) {
+                nidx = (i - 1) * col_num + j;
+                edges_.insert(edge(nodes_[i * col_num + j], nodes_[nidx]));
+            }
+            if (i != row_num - 1) {
+                nidx = (i + 1) * col_num + j;
+                edges_.insert(edge(nodes_[i * col_num + j], nodes_[nidx]));
+            }
+        }
+    }
+
+    WeightMap weightmap = boost::get(boost::edge_weight, g_);
+    edge_descriptor e;
+    bool b;
+    for (auto &it : edges_) {
+        boost::tie(e, b) = boost::add_edge(it.first, it.second, g_);
+        weightmap[e] = 1;
+    }
+}
+
+BoardGraph::~BoardGraph()
+{
+}
+
+void BoardGraph::remove_edges(int node1, int node2)
+{
+    edge_descriptor ed;
+    bool b;
+
+    boost::tie(ed, b) = boost::edge(node1, node2, g_);
+    if (b) {
+        g_.remove_edge(ed);
+    }
+
+    boost::tie(ed, b) = boost::edge(node2, node1, g_);
+    if (b) {
+        g_.remove_edge(ed);
+    }
+
+    edges_.erase(edge(node1, node2));
+    edges_.erase(edge(node2, node1));
+}
+
+bool BoardGraph::find_path(int start_node, int end_node, std::list<int> *path) const
+{
+    std::vector<graph_t::vertex_descriptor> p(boost::num_vertices(g_));
+    std::vector<int> d(boost::num_vertices(g_));
+
+    vertex start = boost::vertex(start_node, g_);
+    vertex end = boost::vertex(end_node, g_);
+
+    try {
+        astar_search(g_, start, boost::astar_heuristic<graph_t, int>(),
+                boost::predecessor_map(&p[0]).distance_map(&d[0]).visitor(astar_goal_visitor<vertex>(end)));
+    }
+    catch (found_goal fg) {
+        for (vertex v = end_node;; v = p[v]) {
+            path->push_front(v);
+            if (p[v] == v)
+                break;
+        }
+
+        std::cout << "shortes path is ";
+
+        std::list<int>::iterator spi = path->begin();
+        std::cout << (start_node / 9) << ":" << (start_node % 9);
+        for (++spi; spi != path->end(); ++spi)
+            std::cout << " -> " << (nodes_[*spi] / 9) << ":"
+                << (nodes_[*spi] % 9);
+        std::cout << std::endl << "Total travel time: " << d[end_node]
+            << std::endl;
+
+        return true;
+    }
+
+    return false;
+}
+
+bool BoardGraph::is_neighbours(int node1, int node2) const
+{
+    return (edges_.count(edge(node1, node2)) > 0);
+}
+
+}  /* namespace Quoridor */
---
+++ src/board_graph.hpp
@@ -0,0 +1,53 @@
+#ifndef QUORIDOR_BOARD_GRAPH_HPP_
+#define QUORIDOR_BOARD_GRAPH_HPP_
+
+#include <vector>
+
+#include <boost/graph/astar_search.hpp>
+#include <boost/graph/adjacency_list.hpp>
+
+namespace Quoridor {
+
+typedef boost::adjacency_list< boost::listS, boost::vecS,
+        boost::undirectedS, boost::no_property,
+        boost::property<boost::edge_weight_t, int> > graph_t;
+typedef boost::property_map<graph_t, boost::edge_weight_t>::type WeightMap;
+typedef graph_t::vertex_descriptor vertex;
+typedef graph_t::edge_descriptor edge_descriptor;
+typedef std::pair<int, int> edge;
+
+struct found_goal {}; // exception for termination
+
+// visitor that terminates when we find the goal
+template <class Vertex>
+class astar_goal_visitor : public boost::default_astar_visitor
+{
+public:
+    astar_goal_visitor(Vertex goal) : m_goal(goal) {}
+    template<class Graph>
+    void examine_vertex(Vertex u, Graph& /* g */) {
+        if (u == m_goal)
+            throw found_goal();
+    }
+private:
+    Vertex m_goal;
+};
+
+class BoardGraph {
+public:
+    BoardGraph(int row_num, int col_num);
+    ~BoardGraph();
+
+    void remove_edges(int node1, int node2);
+    bool find_path(int node1, int node2, std::list<int> *path) const;
+    bool is_neighbours(int node1, int node2) const;
+
+private:
+    graph_t g_;
+    std::vector<int> nodes_;
+    std::set<edge> edges_;
+};
+
+}  /* namespace Quoridor */
+
+#endif  /* QUORIDOR_BOARD_GRAPH_HPP_ */
