a485216d6e63a13edc1bde4e3f602adf6e5f5592
sfod <octosyllabic@gmail.com>
2014-02-02 23:34:33 +0400

    Fix game state.

--- src/game_state.cpp
+++ src/game_state.cpp
@@ -1,4 +1,5 @@
 #include "game_state.hpp"
+#include "main_menu_state.hpp"
 
 namespace Quoridor {
 
@@ -10,8 +11,22 @@ GameState::~GameState()
 {
 }
 
-void GameState::handle_events()
+void GameState::handle_events(StateManager *stm, std::shared_ptr<UI::UIImpl> ui)
 {
+    UI::Event ev;
+    if (ui->poll_event(&ev)) {
+        switch (ev) {
+        case UI::kEnter:
+            break;
+        case UI::kEsc: {
+            std::shared_ptr<Quoridor::IState> menu_state(new Quoridor::MainMenuState());
+            stm->change_state(std::shared_ptr<Quoridor::IState>(menu_state));
+            }
+            break;
+        default:
+            break;
+        }
+    }
 }
 
 void GameState::update()
--- src/main_menu_state.cpp
+++ src/main_menu_state.cpp
@@ -1,4 +1,5 @@
 #include "main_menu_state.hpp"
+#include "game_state.hpp"
 
 namespace Quoridor {
 
@@ -15,7 +16,10 @@ void MainMenuState::handle_events(StateManager *stm, std::shared_ptr<UI::UIImpl>
     UI::Event ev;
     if (ui->poll_event(&ev)) {
         switch (ev) {
-        case UI::kEnter:
+        case UI::kEnter: {
+            std::shared_ptr<Quoridor::IState> game_state(new Quoridor::GameState());
+            stm->change_state(std::shared_ptr<Quoridor::IState>(game_state));
+            }
             break;
         case UI::kEsc:
             stm->stop();
