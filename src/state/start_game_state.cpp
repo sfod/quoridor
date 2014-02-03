106a02dd33fa204cd768bd42959c13827edbc29b
sfod <octosyllabic@gmail.com>
2014-02-03 16:32:31 +0400

    Fix indentation.

--- src/state/start_game_state.cpp
+++ src/state/start_game_state.cpp
@@ -13,16 +13,16 @@ StartGameState::~StartGameState()
 }
 
 void StartGameState::handle_events(StateManager *stm,
-    std::shared_ptr<UI::UIImpl> ui)
+        std::shared_ptr<UI::UIImpl> ui)
 {
     UI::Event ev;
     if (ui->poll_event(&ev)) {
         switch (ev) {
-	case UI::kEnter: {
+        case UI::kEnter: {
             std::shared_ptr<Quoridor::IState> menu_state(new GameState());
             stm->change_state(std::shared_ptr<IState>(menu_state));
-	    }
-	    break;
+            }
+            break;
         case UI::kEsc: {
             std::shared_ptr<Quoridor::IState> menu_state(new MainMenuState());
             stm->change_state(std::shared_ptr<IState>(menu_state));
