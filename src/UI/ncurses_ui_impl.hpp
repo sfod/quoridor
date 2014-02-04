d6cde8cc8cd9547613e763b2098353d2158d1f09
sfod <octosyllabic@gmail.com>
2014-02-04 16:16:27 +0400

    Add ncurses menu.

--- src/UI/ncurses_ui_impl.hpp
+++ src/UI/ncurses_ui_impl.hpp
@@ -1,8 +1,12 @@
 #ifndef QUORIDOR_NCURSES_UI_IMPL_HPP_
 #define QUORIDOR_NCURSES_UI_IMPL_HPP_
 
+#include <memory>
+
 #include <ncurses.h>
+
 #include "ui_impl.hpp"
+#include "ncurses/ncurses_menu.hpp"
 
 namespace Quoridor {
 namespace UI {
@@ -15,8 +19,14 @@ public:
     virtual void draw_window();
     virtual void update(const std::vector<std::vector<char>> &repr);
 
+    virtual void add_menu(const std::vector<std::string> &items);
+    virtual void up_menu();
+    virtual void down_menu();
+    virtual std::string menu_item();
+
 private:
     WINDOW *win_;
+    std::shared_ptr<NcursesMenu> menu_;
 };
 
 }  /* namespace UI */
--- src/UI/ui_impl.hpp
+++ src/UI/ui_impl.hpp
@@ -1,6 +1,7 @@
 #ifndef QUORIDOR_UI_IMPL_HPP_
 #define QUORIDOR_UI_IMPL_HPP_
 
+#include <string>
 #include <vector>
 
 namespace Quoridor {
@@ -12,6 +13,11 @@ public:
 
     virtual void draw_window() = 0;
     virtual void update(const std::vector<std::vector<char>> &repr) = 0;
+
+    virtual void add_menu(const std::vector<std::string> &items) = 0;
+    virtual void up_menu() = 0;
+    virtual void down_menu() = 0;
+    virtual std::string menu_item() = 0;
 };
 
 }  /* namespace UI */
