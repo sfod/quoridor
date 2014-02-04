cd4dd58d890dcaa4cbae7f44329c453fc8d4a1eb
sfod <octosyllabic@gmail.com>
2014-02-04 13:30:51 +0400

    Add NcursesMenu.

---
+++ src/UI/ncurses/ncurses_menu.hpp
@@ -0,0 +1,29 @@
+#ifndef QUORIDOR_NCURSES_MENU_HPP_
+#define QUORIDOR_NCURSES_MENU_HPP_
+
+#include <string>
+#include <vector>
+#include <menu.h>
+
+namespace Quoridor {
+namespace UI {
+
+class NcursesMenu {
+public:
+    NcursesMenu(WINDOW *win, const std::vector<std::string> &items);
+    ~NcursesMenu();
+
+    void up();
+    void down();
+    std::string item();
+
+private:
+    MENU *menu_;
+    ITEM **items_;
+    size_t item_num_;
+};
+
+}  /* namespace UI */
+}  /* namespace Quoridor */
+
+#endif  /* QUORIDOR_NCURSES_MENU_HPP_ */
