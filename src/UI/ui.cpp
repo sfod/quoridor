3951e6f7ac205609b6551799de9613134b0d1425
sfod <octosyllabic@gmail.com>
2014-02-05 17:32:06 +0400

    Fix UI interface.
    - remove all existed methods;
    - add method create_window;
    - add method create_menu;

--- src/UI/ui.cpp
+++ src/UI/ui.cpp
@@ -11,15 +11,5 @@ UI::~UI()
 {
 }
 
-void UI::draw_window()
-{
-    ui_impl_->draw_window();
-}
-
-void UI::update(const std::vector<std::vector<char>> &repr)
-{
-    ui_impl_->update(repr);
-}
-
 }  /* namespace UI */
 }  /* namespace Quoridor */
