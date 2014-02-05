f4e3710b2de603120665f9859ab42c34789c9202
sfod <octosyllabic@gmail.com>
2014-02-05 13:37:21 +0400

    Implement ncurses window.

---
+++ src/UI/ncurses/ncurses_window.hpp
@@ -0,0 +1,29 @@
+#ifndef QUORIDOR_NCURSES_WINDOW_HPP_
+#define QUORIDOR_NCURSES_WINDOW_HPP_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <ncurses.h>
+
+#include "UI/window.hpp"
+
+namespace Quoridor {
+namespace UI {
+
+class NcursesWindow : Window {
+public:
+    NcursesWindow();
+    ~NcursesWindow();
+
+    virtual void draw_board(const std::vector<std::vector<char>> &repr);
+
+private:
+    WINDOW *win_;
+};
+
+}  /* namespace UI */
+}  /* namespace Quoridor */
+
+#endif  /* QUORIDOR_NCURSES_WINDOW_HPP_ */
