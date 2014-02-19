49a921d3f0e65bb8c3e68fb44a97795c4de09840
sfod <octosyllabic@gmail.com>
2014-02-19 14:42:26 +0400

    Remove duplicated include.

--- src/walk_move.hpp
+++ src/walk_move.hpp
@@ -4,8 +4,6 @@
 #include "imove.hpp"
 #include <boost/preprocessor.hpp>
 
-#include <boost/preprocessor.hpp>
-
 #define X_DEFINE_ENUM_WITH_STRING_CONVERSIONS_TOSTRING_CASE(r, data, elem)    \
     case elem : return BOOST_PP_STRINGIZE(elem);
 
