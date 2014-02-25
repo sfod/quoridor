5e886b1c1458aec070483d82667678f7e3de5b0b
sfod <octosyllabic@gmail.com>
2014-02-25 13:55:49 +0400

    Use nodes in WalkMove.

--- src/walk_move.hpp
+++ src/walk_move.hpp
@@ -1,46 +1,22 @@
 #ifndef QUORIDOR_WALK_MOVE_HPP_
 #define QUORIDOR_WALK_MOVE_HPP_
 
+#include "board.hpp"
 #include "imove.hpp"
-#include <boost/preprocessor.hpp>
-
-#define X_DEFINE_ENUM_WITH_STRING_CONVERSIONS_TOSTRING_CASE(r, data, elem)    \
-    case elem : return BOOST_PP_STRINGIZE(elem);
-
-#define DEFINE_ENUM_WITH_STRING_CONVERSIONS(name, enumerators)                \
-    enum name {                                                               \
-        BOOST_PP_SEQ_ENUM(enumerators)                                        \
-    };                                                                        \
-                                                                              \
-    inline const char* ToString(name v)                                       \
-    {                                                                         \
-        switch (v)                                                            \
-        {                                                                     \
-            BOOST_PP_SEQ_FOR_EACH(                                            \
-                X_DEFINE_ENUM_WITH_STRING_CONVERSIONS_TOSTRING_CASE,          \
-                name,                                                         \
-                enumerators                                                   \
-            )                                                                 \
-            default: return "[Unknown " BOOST_PP_STRINGIZE(name) "]";         \
-        }                                                                     \
-    }
 
 namespace Quoridor {
 
 class WalkMove : public IMove {
 public:
     WalkMove();
-    explicit WalkMove(int dir);
+    explicit WalkMove(int node);
     virtual ~WalkMove();
 
-    int dir() const { return dir_; }
-    void set_dir(int dir) { dir_ = dir; }
-
-public:
-    DEFINE_ENUM_WITH_STRING_CONVERSIONS(Direction, (kForward)(kRight)(kBackward)(kLeft)(kEnd))
+    void set_node(int node) { node_ = node; }
+    int node() const { return node_; }
 
 private:
-    int dir_;
+    int node_;
 };
 
 }  /* namespace Quoridor */
