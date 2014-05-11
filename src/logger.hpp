db153f67060a4ba4811d048f725cb01e43308c46
sfod <octosyllabic@gmail.com>
2014-05-11 22:00:29 +0400

    Use custom formatter for file logger

--- src/logger.hpp
+++ src/logger.hpp
@@ -4,6 +4,7 @@
 #include <boost/log/core.hpp>
 #include <boost/log/trivial.hpp>
 #include <boost/log/expressions.hpp>
+#include <boost/log/expressions/keyword.hpp>
 #include <boost/log/expressions/formatters/date_time.hpp>
 #include <boost/log/expressions/formatters/named_scope.hpp>
 #include <boost/log/support/date_time.hpp>
