65d613819a4825fbca990d9695523600acedd32a
sfod <octosyllabic@gmail.com>
2014-05-12 17:44:01 +0400

    Add method IState::name()

--- src/state/istate.hpp
+++ src/state/istate.hpp
@@ -2,6 +2,7 @@
 #define QUORIDOR_ISTATE_HPP_
 
 #include <memory>
+#include <string>
 #include <CEGUI/CEGUI.h>
 #include "state_manager.hpp"
 
@@ -15,6 +16,7 @@ public:
     virtual ~IState() {}
 
     virtual std::shared_ptr<CEGUI::Window> window() const = 0;
+    virtual const std::string &name() const = 0;
 };
 
 }  /* namespace Quoridor */
