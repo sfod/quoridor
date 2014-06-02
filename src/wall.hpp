cc21f25174898c75e914038315ce8a09b7e27d94
sfod <octosyllabic@gmail.com>
2014-06-02 19:10:23 +0400

    Fix Wall
    - use row and col methods instead of line and start_pos;
    - use enum for wall orientation;

--- src/wall.hpp
+++ src/wall.hpp
@@ -7,29 +7,29 @@ class Wall {
 public:
     enum Orientation {
         kHorizontal,
-        kVertical
+        kVertical,
+        kInvalid
     };
 
 public:
     Wall();
-    Wall(Orientation orientation, int line, int start_pos, int cnt);
+    Wall(Orientation orientation, int row, int col, int cnt);
     virtual ~Wall();
 
     void set_orientation(Orientation orientation) { orientation_ = orientation; }
-    void set_line(int line) { line_ = line; }
-    void set_start_pos(int start_pos) { start_pos_ = start_pos; }
+    void set_row(int row) { row_ = row; }
+    void set_col(int col) { col_ = col; }
     void set_cnt(int cnt) { cnt_ = cnt; }
 
     Orientation orientation() const { return orientation_; }
-    int line() const { return line_; }
-    int start_pos() const { return start_pos_; }
-    int end_pos() const { return start_pos_ + cnt_ - 1; }
+    int row() const { return row_; }
+    int col() const { return col_; }
     int cnt() const { return cnt_; }
 
 private:
     Orientation orientation_;
-    int line_;
-    int start_pos_;
+    int row_;
+    int col_;
     int cnt_;
 };
 
