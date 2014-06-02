b6ba9474e6ca3ce7253a2e39a649f8570e472bdf
sfod <octosyllabic@gmail.com>
2014-06-02 19:11:51 +0400

    Move logic from Board to Game

--- src/game.cpp
+++ src/game.cpp
@@ -5,8 +5,8 @@ namespace Quoridor {
 
 static const std::vector<int> pawn_idx_list = {0, 2, 1, 3};
 
-Game::Game(size_t board_size) : board_size_(board_size), pawn_data_list_(),
-    cur_pawn_idx_(-1)
+Game::Game(int board_size) : board_size_(board_size), pawn_data_list_(),
+    cur_pawn_idx_(-1), bg_(board_size_, board_size_), wg_(board_size_ + 1)
 {
 }
 
@@ -31,7 +31,7 @@ void Game::set_pawns(std::vector<std::shared_ptr<Pawn>> &pawn_list)
         case 0:
             pawn_data.node.set_row(0);
             pawn_data.node.set_col(board_size_ / 2);
-            for (size_t j = 0; j < board_size_; ++j) {
+            for (int j = 0; j < board_size_; ++j) {
                 Node gn(board_size_ - 1, j);
                 pawn_data.goal_nodes.insert(gn);
             }
@@ -39,7 +39,7 @@ void Game::set_pawns(std::vector<std::shared_ptr<Pawn>> &pawn_list)
         case 1:
             pawn_data.node.set_row(board_size_ / 2);
             pawn_data.node.set_col(0);
-            for (size_t j = 0; j < board_size_; ++j) {
+            for (int j = 0; j < board_size_; ++j) {
                 Node gn(j, board_size_ - 1);
                 pawn_data.goal_nodes.insert(gn);
             }
@@ -47,7 +47,7 @@ void Game::set_pawns(std::vector<std::shared_ptr<Pawn>> &pawn_list)
         case 2:
             pawn_data.node.set_row(board_size_ - 1);
             pawn_data.node.set_col(board_size_ / 2);
-            for (size_t j = 0; j < board_size_; ++j) {
+            for (int j = 0; j < board_size_; ++j) {
                 Node gn(0, j);
                 pawn_data.goal_nodes.insert(gn);
             }
@@ -55,7 +55,7 @@ void Game::set_pawns(std::vector<std::shared_ptr<Pawn>> &pawn_list)
         case 3:
             pawn_data.node.set_row(board_size_ / 2);
             pawn_data.node.set_col(board_size_ - 1);
-            for (size_t j = 0; j < board_size_; ++j) {
+            for (int j = 0; j < board_size_; ++j) {
                 Node gn(j, 0);
                 pawn_data.goal_nodes.insert(gn);
             }
@@ -75,16 +75,159 @@ std::shared_ptr<Pawn> Game::next_pawn()
     return it->second.pawn;
 }
 
-int Game::move_pawn(std::shared_ptr<Pawn> pawn, const WalkMove &move)
+int Game::move_pawn(const WalkMove &move)
 {
+    Node cur_node = pawn_data_list_[cur_pawn_idx_].node;
+    Node node = move.node();
+
+    if (bg_.is_adjacent(cur_node, node)) {
+        bg_.unblock_neighbours(cur_node);
+        pawn_data_list_[cur_pawn_idx_].node = node;
+        bg_.block_neighbours(node);
+        return 0;
+    }
+    else {
+        return -1;
+    }
 }
 
-int Game::add_wall(std::shared_ptr<Pawn> pawn, const WallMove &move)
+int Game::add_wall(const WallMove &move)
 {
+    const Wall &wall = move.wall();
+    std::vector<std::pair<Node, Node>> edges;
+
+    if (try_add_wall(wall, &edges) < 0) {
+        return -1;
+    }
+
+    wg_.apply_tmp_wall();
+    for (auto edge : edges) {
+        bg_.remove_edges(edge.first, edge.second);
+    }
+
+    return 0;
+}
+
+int Game::try_add_wall(const Wall &wall,
+        std::vector<std::pair<Node, Node>> *edges)
+{
+    if (wg_.add_tmp_wall(wall) < 0) {
+        return -1;
+    }
+
+    bg_.reset_filters();
+
+    Node node1;
+    Node node2;
+    Node node_tmp;
+    if (wall.orientation() == Wall::kHorizontal) {
+        for (int i = 0; i < wall.cnt(); ++i) {
+            node1 = Node(wall.row() - 1, wall.col() + i);
+            node2 = Node(wall.row(), wall.col() + i);
+            bg_.filter_edges(node1, node2);
+            edges->push_back(std::make_pair(node1, node2));
+
+            node_tmp = Node(wall.row() + 1, wall.col() + i);
+            if ((node_tmp.row() >= 0) && (node_tmp.row() < board_size_)
+                    && (node_tmp.col() >= 0) && (node_tmp.col() < board_size_)) {
+                bg_.filter_edges(node1, node_tmp);
+                edges->push_back(std::make_pair(node1, node_tmp));
+            }
+
+            node_tmp = Node(wall.row() - 2, wall.col() + i);
+            if ((node_tmp.row() >= 0) && (node_tmp.row() < board_size_)
+                    && (node_tmp.col() >= 0) && (node_tmp.col() < board_size_)) {
+                bg_.filter_edges(node_tmp, node2);
+                edges->push_back(std::make_pair(node_tmp, node2));
+            }
+
+            for (int j = i - 1; j <= i + 1; j += 2) {
+                node_tmp = Node(wall.row(), wall.col() + j);
+                if ((node_tmp.row() >= 0) && (node_tmp.row() < board_size_)
+                        && (node_tmp.col() >= 0) && (node_tmp.col() < board_size_)) {
+                    bg_.filter_edges(node1, node_tmp);
+                    edges->push_back(std::make_pair(node1, node_tmp));
+                }
+
+                node_tmp = Node(wall.row() - 1, wall.col() + j);
+                if ((node_tmp.row() >= 0) && (node_tmp.row() < board_size_)
+                        && (node_tmp.col() >= 0) && (node_tmp.col() < board_size_)) {
+                    bg_.filter_edges(node_tmp, node2);
+                    edges->push_back(std::make_pair(node_tmp, node2));
+                }
+            }
+        }
+    }
+    else {
+        for (int i = 0; i < wall.cnt(); ++i) {
+            node1 = Node(wall.row() + i, wall.col() - 1);
+            node2 = Node(wall.row() + i, wall.col());
+            bg_.filter_edges(node1, node2);
+            edges->push_back(std::make_pair(node1, node2));
+
+            node_tmp = Node(wall.row() + i, wall.col() + 1);
+            if ((node_tmp.row() >= 0) && (node_tmp.row() < board_size_)
+                    && (node_tmp.col() >= 0) && (node_tmp.col() < board_size_)) {
+                bg_.filter_edges(node1, node_tmp);
+                edges->push_back(std::make_pair(node1, node_tmp));
+            }
+
+            node_tmp = Node(wall.row() + i, wall.col() - 2);
+            if ((node_tmp.row() >= 0) && (node_tmp.row() < board_size_)
+                    && (node_tmp.col() >= 0) && (node_tmp.col() < board_size_)) {
+                bg_.filter_edges(node_tmp, node2);
+                edges->push_back(std::make_pair(node_tmp, node2));
+            }
+
+            for (int j = i - 1; j <= i + 1; j += 2) {
+                node_tmp = Node(wall.row() + j, wall.col());
+                if ((node_tmp.row() >= 0) && (node_tmp.row() < board_size_)
+                        && (node_tmp.col() >= 0) && (node_tmp.col() < board_size_)) {
+                    bg_.filter_edges(node1, node_tmp);
+                    edges->push_back(std::make_pair(node1, node_tmp));
+                }
+
+                node_tmp = Node(wall.row() + j, wall.col() - 1);
+                if ((node_tmp.row() >= 0) && (node_tmp.row() < board_size_)
+                        && (node_tmp.col() >= 0) && (node_tmp.col() < board_size_)) {
+                    bg_.filter_edges(node_tmp, node2);
+                    edges->push_back(std::make_pair(node_tmp, node2));
+                }
+            }
+        }
+    }
+
+    bool path_blocked = false;
+
+    for (auto pawn_data : pawn_data_list_) {
+        path_blocked = true;
+        for (auto goal_node : pawn_data.second.goal_nodes) {
+            if (bg_.is_path_exists(pawn_data.second.node, goal_node)) {
+                path_blocked = false;
+                break;
+            }
+        }
+
+        // wall blocks all pathes to the opposite side for one of pawns
+        if (path_blocked) {
+            break;
+        }
+    }
+
+    if (path_blocked) {
+        return -1;
+    }
+
+    return 0;
 }
 
 bool Game::is_finished() const
 {
+    Node cur_node = pawn_data_list_.at(cur_pawn_idx_).node;
+    if (pawn_data_list_.at(cur_pawn_idx_).goal_nodes.count(cur_node) != 0) {
+        return true;
+    }
+    return false;
 }
 
 }  // namespace Quoridor
--- src/game.hpp
+++ src/game.hpp
@@ -6,36 +6,44 @@
 #include <set>
 #include <vector>
 
+#include "board_graph.hpp"
 #include "node.hpp"
 #include "pawn.hpp"
 #include "walk_move.hpp"
+#include "wall_grid.hpp"
 #include "wall_move.hpp"
 
 namespace Quoridor {
 
 struct pawn_data_t {
     std::shared_ptr<Pawn> pawn;
-    Node start_node;
+    Node node;
     std::set<Node> goal_nodes;
 };
 
 class Game {
 public:
-    explicit Game(size_t board_size);
+    explicit Game(int board_size);
     virtual ~Game();
 
     void set_pawns(std::vector<std::shared_ptr<Pawn>> &pawn_list);
     std::shared_ptr<Pawn> next_pawn();
 
-    int move_pawn(std::shared_ptr<Pawn> pawn, const WalkMove &move);
-    int add_wall(std::shared_ptr<Pawn> pawn, const WallMove &move);
+    int move_pawn(const WalkMove &move);
+    int add_wall(const WallMove &move);
 
-    bool is_finished(std::shared_ptr<Pawn> pawn) const;
+    bool is_finished() const;
 
 private:
-    size_t board_size_;
+    int try_add_wall(const Wall &wall,
+            std::vector<std::pair<Node, Node>> *edges);
+
+private:
+    int board_size_;
     std::map<int, pawn_data_t> pawn_data_list_;
     int cur_pawn_idx_;
+    BoardGraph bg_;
+    WallGrid wg_;
 };
 
 }  // namespace Quoridor
