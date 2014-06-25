8801fb7487d15422fe8155b02af30aba07ec8f08
sfod <octosyllabic@gmail.com>
2014-06-25 13:43:34 +0400

    Add couple unit-tests for BoardGraph

--- t/board_graph_unittest.cpp
+++ t/board_graph_unittest.cpp
@@ -207,6 +207,75 @@ TEST_F(BoardGraphTest, unblock_node_near_blocked_node)
     EXPECT_FALSE(bg_.is_adjacent(Node(2, 3), Node(1, 2)));
 }
 
+TEST_F(BoardGraphTest, block_node_near_blocked_node_2)
+{
+    bg_.block_node(Node(2, 2));
+    bg_.block_node(Node(2, 3));
+
+    // edges from the blocked node to neighbour nodes
+    EXPECT_TRUE(bg_.is_adjacent(Node(2, 2), Node(1, 2)));
+    EXPECT_TRUE(bg_.is_adjacent(Node(2, 2), Node(2, 1)));
+    EXPECT_TRUE(bg_.is_adjacent(Node(2, 2), Node(3, 2)));
+    EXPECT_FALSE(bg_.is_adjacent(Node(2, 2), Node(2, 3)));
+
+    // edges from neighbour nodes to the blocked node
+    EXPECT_FALSE(bg_.is_adjacent(Node(1, 2), Node(2, 2)));
+    EXPECT_FALSE(bg_.is_adjacent(Node(2, 1), Node(2, 2)));
+    EXPECT_FALSE(bg_.is_adjacent(Node(3, 2), Node(2, 2)));
+    EXPECT_FALSE(bg_.is_adjacent(Node(2, 3), Node(2, 2)));
+
+    // nodes across the blocked node
+    EXPECT_TRUE(bg_.is_adjacent(Node(1, 2), Node(3, 2)));
+    EXPECT_FALSE(bg_.is_adjacent(Node(2, 1), Node(2, 3)));
+    EXPECT_TRUE(bg_.is_adjacent(Node(3, 2), Node(1, 2)));
+    EXPECT_FALSE(bg_.is_adjacent(Node(2, 3), Node(2, 1)));
+
+    // diagonal nodes
+    EXPECT_FALSE(bg_.is_adjacent(Node(1, 2), Node(2, 1)));
+    EXPECT_FALSE(bg_.is_adjacent(Node(1, 2), Node(2, 3)));
+    EXPECT_TRUE(bg_.is_adjacent(Node(2, 1), Node(3, 2)));
+    EXPECT_TRUE(bg_.is_adjacent(Node(2, 1), Node(1, 2)));
+    EXPECT_FALSE(bg_.is_adjacent(Node(3, 2), Node(2, 1)));
+    EXPECT_FALSE(bg_.is_adjacent(Node(3, 2), Node(2, 3)));
+    EXPECT_FALSE(bg_.is_adjacent(Node(2, 3), Node(3, 2)));
+    EXPECT_FALSE(bg_.is_adjacent(Node(2, 3), Node(1, 2)));
+}
+
+TEST_F(BoardGraphTest, unblock_node_near_blocked_node_2)
+{
+    bg_.block_node(Node(2, 2));
+    bg_.block_node(Node(2, 3));
+    bg_.unblock_node(Node(2, 2));
+
+    // edges from the blocked node to neighbour nodes
+    EXPECT_TRUE(bg_.is_adjacent(Node(2, 2), Node(1, 2)));
+    EXPECT_TRUE(bg_.is_adjacent(Node(2, 2), Node(2, 1)));
+    EXPECT_TRUE(bg_.is_adjacent(Node(2, 2), Node(3, 2)));
+    EXPECT_FALSE(bg_.is_adjacent(Node(2, 2), Node(2, 3)));
+
+    // edges from neighbour nodes to the blocked node
+    EXPECT_TRUE(bg_.is_adjacent(Node(1, 2), Node(2, 2)));
+    EXPECT_TRUE(bg_.is_adjacent(Node(2, 1), Node(2, 2)));
+    EXPECT_TRUE(bg_.is_adjacent(Node(3, 2), Node(2, 2)));
+    EXPECT_TRUE(bg_.is_adjacent(Node(2, 3), Node(2, 2)));
+
+    // nodes across the blocked node
+    EXPECT_FALSE(bg_.is_adjacent(Node(1, 2), Node(3, 2)));
+    EXPECT_FALSE(bg_.is_adjacent(Node(2, 1), Node(2, 3)));
+    EXPECT_FALSE(bg_.is_adjacent(Node(3, 2), Node(1, 2)));
+    EXPECT_FALSE(bg_.is_adjacent(Node(2, 3), Node(2, 1)));
+
+    // diagonal nodes
+    EXPECT_FALSE(bg_.is_adjacent(Node(1, 2), Node(2, 1)));
+    EXPECT_FALSE(bg_.is_adjacent(Node(1, 2), Node(2, 3)));
+    EXPECT_FALSE(bg_.is_adjacent(Node(2, 1), Node(3, 2)));
+    EXPECT_FALSE(bg_.is_adjacent(Node(2, 1), Node(1, 2)));
+    EXPECT_FALSE(bg_.is_adjacent(Node(3, 2), Node(2, 1)));
+    EXPECT_FALSE(bg_.is_adjacent(Node(3, 2), Node(2, 3)));
+    EXPECT_FALSE(bg_.is_adjacent(Node(2, 3), Node(3, 2)));
+    EXPECT_FALSE(bg_.is_adjacent(Node(2, 3), Node(1, 2)));
+}
+
 TEST_F(BoardGraphTest, block_node_adjacent_to_border)
 {
     bg_.block_node(Node(1, 0));
