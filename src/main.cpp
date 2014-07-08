dd6abca37e10567a2cb9bef8f3ef22dace937ea4
sfod <octosyllabic@gmail.com>
2014-07-08 19:57:25 +0400

    Check tag_attr existence in log formatter

--- src/main.cpp
+++ src/main.cpp
@@ -87,16 +87,17 @@ static int init(int argc, char **argv)
 
 void formatter(boost::log::record_view const &rec, boost::log::formatting_ostream &strm)
 {
-    strm << rec[timestamp]
-        << " [" << rec[severity] << "] ";
+    strm << rec[timestamp] << " [" << rec[severity] << "] ";
 
-    boost::log::attributes::named_scope_list scope_list = rec[scope].get();
-    for (auto iter = scope_list.begin(); iter != scope_list.end(); ++iter) {
-        strm << "(" << iter->scope_name << ") ";
+    for (const auto &n : rec[scope].get()) {
+        strm << "(" << n.scope_name << ") ";
     }
 
-    strm << "(" << rec[tag_attr] << ") ";
-    strm << rec[boost::log::expressions::smessage];
+    if (rec[tag_attr]) {
+        strm << "(" << rec[tag_attr] << ") ";
+    }
+
+    strm << rec[blexpr::smessage];
 }
 
 static void init_logging(const std::string &logfile)
@@ -115,9 +116,13 @@ static void init_logging(const std::string &logfile)
     boost::log::add_console_log(
         std::clog,
         boost::log::keywords::format = (
-            boost::log::expressions::stream
-                << "[" << severity << "] (" << tag_attr << ") "
-                << boost::log::expressions::smessage
+            blexpr::stream
+                << "[" << severity << "] "
+                << blexpr::if_(blexpr::has_attr(tag_attr))
+                    [
+                        blexpr::stream << "(" << tag_attr << ") "
+                    ]
+                << blexpr::smessage
         )
     );
 
